7a5dfe429b4ccbd1c7adb75ec20064374b26be14
shadrqen <shadrqen@outlook.com>
2022-05-19 16:39:54 +0300

    WIP: triplets return ongoing

---
+++ src/m/3Sum.ts
@@ -0,0 +1,42 @@
+/**
+ * algorithm
+ * 1 Loop through array
+ * 2 Pick current element A
+ * 3 Loop through the next elements from the next one
+ * 4 Pick current element B
+ * 5 Loop through the next elements from the next next one
+ * 6 Pick current element C
+ * 7 Add A+B+C and see whether A+B+C = 0
+ * 8 If it does, push the array [A, B, C] into the return array
+ * 9 Repeat from point 2
+ * @param {array[][]} nums
+ */
+
+export function threeSum (nums: number[]): number[] | number[][] {
+  if (nums.length > 2) {
+    let count = 0
+    const sumArray = []
+    const coveredIndices: number[] = []
+    while (count < nums.length) {
+      let layer2Count = count + 1
+      while (layer2Count < nums.length) {
+        let layer3Count = layer2Count + 1
+        while (layer3Count < nums.length) {
+          const sumIsZero = nums[count] + nums[layer2Count] + nums[layer3Count] === 0
+          const indexCovered: boolean = [count, layer2Count, layer3Count].every(num => coveredIndices.includes(num))
+          if (sumIsZero && !indexCovered) {
+            sumArray.push([nums[count], nums[layer2Count], nums[layer3Count]])
+            coveredIndices.push(count, layer2Count, layer3Count)
+          }
+          layer3Count++
+        }
+        layer2Count++
+      }
+      count++
+    }
+    console.log(sumArray)
+    return sumArray
+  } else {
+    return []
+  }
+}
---
+++ tests/3Sum.spec.ts
@@ -0,0 +1,13 @@
+import { threeSum } from '~/src/m/3Sum'
+
+describe('3Sum', function () {
+  it('should return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0', function () {
+    const output1 = threeSum([-1, 0, 1, 2, -1, -4])
+    const outputEmpty = threeSum([])
+    const outputOneElement = threeSum([0])
+
+    expect(output1).toStrictEqual([[-1, -1, 2], [-1, 0, 1]])
+    expect(outputEmpty).toStrictEqual([[]])
+    expect(outputOneElement).toStrictEqual([[]])
+  })
+})
