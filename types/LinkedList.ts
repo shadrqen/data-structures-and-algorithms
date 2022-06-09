a5aab70711c54418c7582e7d2188115a45c15597
Rotiken Gisa <shadrqen@outlook.com>
2022-06-10 00:36:18 +0300

    feat: add doubly-linked-list

--- types/LinkedList.ts
+++ types/LinkedList.ts
@@ -3,10 +3,24 @@ export interface SinglyNode {
   next?: SinglyNode | null
 }
 
+export interface DoublyNode {
+  value: number | null
+  previous?: DoublyNode | null
+  next?: DoublyNode | null
+}
+
+export type DoublyNodeUnion = DoublyNode | null
+
 export type SinglyNodeUnion = SinglyNode | null
 
 export interface SinglyList {
   head: SinglyNodeUnion
+  size: number
+}
+
+export interface DoublyList {
+  head: DoublyNodeUnion
+  tail: DoublyNodeUnion
   size: number
 }
 
