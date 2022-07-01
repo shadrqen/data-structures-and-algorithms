a8db9401a852c49f7d66030bc891e6b5f803c3e7
shadrqen <shadrqenpers@outlook.com>
2022-07-01 22:51:51 +0300

    feat: add reversing string

---
+++ tests/ReverseString.spec.ts
@@ -0,0 +1,12 @@
+import { reverseString } from '~/src/ReverseString'
+
+describe('Reverse String', function () {
+  it('should reverse string', () => {
+    const initialString: string = 'password'
+    const expectedFinalString: string = 'drowssap'
+
+    const reversedString: string = reverseString(initialString)
+
+    expect(reversedString).toBe(expectedFinalString)
+  })
+})
