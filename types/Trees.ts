cfde5b6f9fc1fdbeda493fd27dfaf6144ca15875
shadrqen <shadrqenpers@outlook.com>
2022-07-07 00:46:21 +0300

    feat: add BST breadth-first-search

--- types/Trees.ts
+++ types/Trees.ts
@@ -3,3 +3,20 @@ export interface BSTNode {
     left: BSTNode | null
     right: BSTNode | null
 }
+
+export type BSTNodeUnion = BSTNode | null
+
+export interface BSTInterface {
+    root: BSTNodeUnion
+    inOrderTraversedValues: number[]
+    preOrderTraversedValues: number[]
+    postOrderTraversedValues: number[]
+    insert (value: number): void
+    insertWNE (node: BSTNode, value: number): void
+    search (node: BSTNodeUnion, value: number) : boolean
+    remove (value: number, sucessorOrPredecessor: string | null): BSTNodeUnion
+    inorderTraversal (node: BSTNodeUnion): void
+    preorderTraversal (node: BSTNodeUnion): void
+    postorderTraversal (node: BSTNodeUnion): void
+    bfs(node: BSTNodeUnion, callback: Function): void
+}
