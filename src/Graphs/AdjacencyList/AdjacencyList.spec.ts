4b86058ee7406b70ff93962f357b4f570c33f300
shadrqen <shadrqenpers@outlook.com>
2022-07-07 12:45:54 +0300

    feat: add adjacency list

--- src/Graphs/AdjacencyList/AdjacencyList.spec.ts
+++ src/Graphs/AdjacencyList/AdjacencyList.spec.ts
@@ -1,60 +1,64 @@
 import { Graph } from '~/src/Graphs/AdjacencyList/AdjacencyList'
 
 describe('Graph using Adjacency List', function () {
-  it('should add vertices', () => {
+  const createGraph = (vertexes: string[], edges: string[][] = []): Graph => {
     const graph = new Graph()
 
-    // add vertexes
-    graph.addVertex('A')
-    graph.addVertex('B')
-    graph.addVertex('C')
+    vertexes.forEach(vertex => {
+      graph.addVertex(vertex)
+    })
 
-    expect(graph.adjacentList.has('A')).toBe(true)
-    expect(graph.adjacentList.has('B')).toBe(true)
-    expect(graph.adjacentList.has('C')).toBe(true)
+    if (edges.length > 0) {
+      edges.forEach(edge => {
+        graph.addEdge(edge[0], edge[1])
+      })
+    }
+    return graph
+  }
+
+  it('should add vertices', () => {
+    const vertexes: string[] = ['A', 'B', 'C']
+    const graph: Graph = createGraph(vertexes)
+
+    expect(graph.adjacentList.has(vertexes[0])).toBe(true)
+    expect(graph.adjacentList.has(vertexes[1])).toBe(true)
+    expect(graph.adjacentList.has(vertexes[2])).toBe(true)
   })
   it('should add edge', () => {
-    const graph = new Graph()
+    const vertexes: string[] = ['A', 'B', 'C']
+    const edges: string[][] = [['A', 'B'], ['B', 'C']]
 
-    // add vertexes
-    graph.addVertex('A')
-    graph.addVertex('B')
-    graph.addVertex('C')
+    const graph: Graph = createGraph(vertexes, edges)
 
-    // add edges
-    graph.addEdge('A', 'B')
-    graph.addEdge('B', 'C')
-
-    /* TODO: To implement the test below
-    * expect(graph.adjacentList.values()).toContain('A') */
+    /* TODO: To implement the test to check fo */
+    expect(graph.adjacentList.keys()).toBeTruthy()
   })
   it('should search using bfs', () => {
-    const graph = new Graph()
+    const vertexes: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
-    // add vertexes
-    graph.addVertex('A')
-    graph.addVertex('B')
-    graph.addVertex('C')
-    graph.addVertex('D')
-    graph.addVertex('E')
-    graph.addVertex('F')
-    graph.addVertex('G')
-    graph.addVertex('H')
+    const edges: string[][] = [['A', 'B'], ['A', 'D'], ['A', 'E'], ['B', 'C'], ['C', 'D'],
+      ['C', 'F'], ['C', 'G'], ['D', 'E'], ['E', 'F'], ['F', 'G'], ['G', 'H']]
 
-    // add edges
-    graph.addEdge('A', 'B')
-    graph.addEdge('A', 'D')
-    graph.addEdge('A', 'E')
-    graph.addEdge('B', 'C')
-    graph.addEdge('C', 'D')
-    graph.addEdge('C', 'F')
-    graph.addEdge('C', 'G')
-    graph.addEdge('D', 'E')
-    graph.addEdge('E', 'F')
-    graph.addEdge('F', 'G')
-    graph.addEdge('G', 'H')
+    const graph: Graph = createGraph(vertexes, edges)
 
-    graph.bfs('A')
+    // graph.bfs('A')
+
+    /* TODO: To implement appropriate test */
+    expect(graph.adjacentList).toBeTruthy()
+  })
+  it('should respond to starting node not found using bfs', () => {
+    const vertexes: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+    const edges: string[][] = [['A', 'B'], ['A', 'D'], ['A', 'E'], ['B', 'C'], ['C', 'D'],
+      ['C', 'F'], ['C', 'G'], ['D', 'E'], ['E', 'F'], ['F', 'G']]
+
+    const graph: Graph = createGraph(vertexes, edges)
+    const missingVertex: string = 'K'
+
+    graph.bfs(missingVertex)
+
+    /* TODO: To implement appropriate test */
+    expect(graph.adjacentList).toBeTruthy()
   })
   it('should search using dfs', () => {})
 })
--- types/Graphs.ts
+++ types/Graphs.ts
@@ -2,7 +2,7 @@ export interface GraphInterface {
     noOfVertices: number
     adjacentList: Map<any, any>
     addVertex(vertex: string): void
-    addEdge(vertex1: string, vertex2: string): void
+    addEdge(vertex1: string, vertex2: string, directed?: boolean): void
     printGraph(): void
     bfs(startingVertex: string): void
 }
